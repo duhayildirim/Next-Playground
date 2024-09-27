63410f32bd9be62a26da56c6a058ad9fde4dafd1
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2024-09-27 04:29:16 +0300

    team page

---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Team() {
+    return (
+        <div>
+            <h1>Who are you?</h1>
+            I’m Duha a guy who likes to develop things..
+        </div>
+    )
+}
+
+export default Team
