dc3a03831d0a5fd2d76bf105ab1a5fe9a4d79e0b
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2024-09-30 03:57:40 +0300

    update about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,19 +1,18 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 function About() {
     return (
-        <div>
+        <div className="content-container">
             <h1>What is this?</h1>
             <h3>The Rick and Morty API is a REST(ish) and GraphQL API based on the television show Rick and Morty.
                 You will have access to about hundreds of characters, images, locations and episodes.
-                The Rick and Morty API is filled with canonical information as seen on the TV show.
-            </h3>
+                The Rick and Morty API is filled with canonical information as seen on the TV show.</h3>
             <Link className="btn" href="/about/team">
                 Who are you?
             </Link>
         </div>
-    )
+    );
 }
 
 export default About;
