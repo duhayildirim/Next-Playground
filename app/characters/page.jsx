175a00ad0e8d11789deb0c61160a19fe2fbebca7
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2024-10-17 10:10:33 +0300

    client server

--- app/characters/page.jsx
+++ app/characters/page.jsx
@@ -1,17 +1,55 @@
-import Image from "next/image";
+"use client"
 
-async function getCharacters() {
-    const res = await fetch('http://localhost:3000/api/characters')
-    return res.json();
-}
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Loading from "../loading";
+
+// async function getCharacters() {
+//     const res = await fetch('http://localhost:3000/api/characters')
+//     return res.json();
+// }
 
 async function Characters() {
-    const characters = await getCharacters();
+    // const characters = await getCharacters();
+
+    const [page, setPage] = useState(1);
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        const getCharacters = async (page) => {
+            const res = await fetch('http://localhost:3000/api/characters?page=' + page);
+            const data = await res.json();
+            setCharacters(data);
+
+            setLoading(false)
+        }
+
+        getCharacters(page)
+    }, [page])
+
+    
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        setPage(page + 1);
+    }
+
+    const handleBack = (e) => {
+        e.preventDefault();
+        setPage(page - 1);
+    }
+
+    if (loading) {
+        return <Loading />
+    }
 
     return (
         <>
             <div className="content-container">
                 <h3>All Characters of Rick and Morty</h3>
+                <p><small>Page {page}</small></p>
             </div>
             <div className="card-container">
                 {characters.results.map((cha) => (
@@ -24,6 +62,10 @@ async function Characters() {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="content-container">
+                {characters.info.prev && <button className="btn" onClick={handleBack}>Back</button>}
+                {characters.info.next && <button className="btn" onClick={handleNext}>Next</button>}
             </div>
         </>
 
